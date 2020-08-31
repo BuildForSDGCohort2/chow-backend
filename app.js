179ab31c5ebc36ffc85bfbc63a07067a3fdc4f5b
Kingsley <kingsleyumujeyan@gmail.com>
2020-08-31 18:07:10 +0100

    ft--server created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', (req, res) => res.status(200).json({
+  status: true,
+  message: 'Chow is working!',
+}));
+
+module.exports = app;
