bfda77d9125b6ff2c6fb12cb721adf524cb3e727
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-08 21:40:30 +0100

    ft--authentication middleware created

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,20 +2,21 @@ const jwt = require('jsonwebtoken');
 
 function checkAuth(req, res, next) {
   const token = req.header('token');
-  if(!token)
+  if (!token) {
     return res.status(401).json({
-        message: 'Autth Error'
+        message: 'Autth Error',
     });
+  }
   try {
     const decoded = jwt.verify(token, 'randomString');
     req.user = decoded.user;
     next();
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     res.status(500).send({
-        message: 'Invalid Token'
+      message: 'Invalid Token',
     });
   }
-};
+}
 
 module.exports = checkAuth;
