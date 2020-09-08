3a5fabc4d737843f51fad16937e17149e2ed154b
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-08 06:54:40 +0100

    ft--initialise db in server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const initDb = require('./config/db');
 
+initDb();
 const app = express();
 
 app.use(cors());
