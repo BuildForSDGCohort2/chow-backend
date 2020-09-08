a7f5e31bd875c999e7646d4b16162d2e15628beb
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-08 05:43:40 +0100

    bg--database created,codacy

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-const password = "XXWBmVylJM9873ps";
-const username = "chow";
-const dbname = "chow";
+const password = 'XXWBmVylJM9873ps';
+const username = 'chow';
+const dbname = 'chow';
 const MONGO_URI = `mongodb+srv://${username}:${password}@cluster0-x8poy.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 const InitDb = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true    
+      useNewUrlParser: true, 
     });
-    console.log("Connected to DB successfully!");
+    //console.log("Connected to DB successfully!");
   } catch (e) {
-    console.log(e);
+    //console.log(e);
     throw e;
   }
 };
