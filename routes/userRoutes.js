c6ac87750278ac26e35d7590b8afac5975138df6
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-08 20:14:06 +0100

    ft--user signup route created

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { signUp } = require('../controllers/userController');
+
+/**
+ * @description - User SignUp
+ * @route - POST /api/v1/signup
+ * @access - public
+ */
+ router.post('/signup', signUp);
+
+ export default router;
