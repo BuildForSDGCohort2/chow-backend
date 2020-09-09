faa8bb084ff41db102fda626ae24af747376c3e7
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-09 17:41:17 +0100

    bg--codacy

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,26 +35,26 @@ const User = require('../models/userModel');
         password
     });  
     const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(password, salt);  
+    newUser.password = await bcrypt.hash(password, salt);
     await newUser.save();  
     const payload = {
-        newUser: {
-            id: newUser.id
-        }
+      newUser: {
+        id: newUser.id
+      }
     };  
     jwt.sign(
-        payload,
-        'randomString', {
-            expiresIn: 10000
-        },
-        (err, token) => {
-            if(err) throw err;
-            return res.status(200).json({
-                status: true,
-                message: 'user signup successful',
-                token
-            });
-        }
+      payload,
+      'randomString', {
+          expiresIn: 10000
+      },
+      (err, token) => {
+        if(err) throw err;
+        return res.status(200).json({
+            status: true,
+            message: 'user signup successful',
+            token
+        });
+      }
     );
   } catch (err) {
         // console.log(err.message);
@@ -67,87 +67,87 @@ const User = require('../models/userModel');
  */
 
 const signIn = async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        });
-    }
-
-    const { email, password } = req.body;
-    try {
-      let user = await User.findOne({
-          email
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+      return res.status(400).json({
+          errors: errors.array()
       });
-      if(!user)
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({
+        email
+    });
+    if(!user)
+      return res.status(400).json({
+          message: 'User does not Exist!'
+      });
+      const isMatch = await bcrypt.compare(password, user.password);
+      if(!isMatch)
         return res.status(400).json({
-            message: 'User does not Exist!'
+            message: 'Incorrect Password !'
         });
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch)
-          return res.status(400).json({
-              message: 'Incorrect Password !'
-          });
-        const payload = {
-          user: {
-              id: user.id
+      const payload = {
+        user: {
+            id: user.id
+        }
+      };
+      jwt.sign(
+          payload,
+          'randomString',
+          {
+              expiresIn: 3000
+          },
+          (err, token) => {
+            if(err) throw err;
+            return res.status(200).json({
+                status: true,
+                message: 'User signin successful',
+                token
+            });
           }
-        };
-        jwt.sign(
-            payload,
-            'randomString',
-            {
-                expiresIn: 3000
-            },
-            (err, token) => {
-              if(err) throw err;
-              return res.status(200).json({
-                  status: true,
-                  message: 'User signin successful',
-                  token
-              });
-            }
-        );
-    } catch (error) {
+      );
+  } catch (error) {
       console.error(error);
       res.status(500).json({
         message: 'Server Error'
       });
-    }
+  }
 }
 
 /**
  * @description - get user by Token
  */
 const getUserByToken = (req, res) => {
-    const currentUserId = req.authUser.id;
-    const filter = {
-        _id: currentUserId,
-    }
-    User.findOne(filter, '-password', (err, user) => {
-        if (err)
-          return res.status(500).json({
-              status: false,
-              error: 'Server error:: Could not retrieve record' 
-          });
-        if (!user)
-          return res.status(400).json({
-              status: false,
-              error: 'No such record found'
-          });
-        // User found
-        const data = {
-            id: user._id,
-            username: user.username,
-            email: user.email,
-            auth: user.auth
-        };
-        return res.status(200).json({
-            status: true,
-            message: 'User records',
-            data
+  const currentUserId = req.authUser.id;
+  const filter = {
+      _id: currentUserId,
+  }
+  User.findOne(filter, '-password', (err, user) => {
+      if (err)
+        return res.status(500).json({
+            status: false,
+            error: 'Server error:: Could not retrieve record' 
         });
-    });
+      if (!user)
+        return res.status(400).json({
+            status: false,
+            error: 'No such record found'
+        });
+      // User found
+      const data = {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          auth: user.auth
+      };
+      return res.status(200).json({
+          status: true,
+          message: 'User records',
+          data
+      });
+  });
 };
  module.exports = {
      signUp,
