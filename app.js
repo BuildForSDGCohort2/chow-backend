fd975cf733e9fb3a1d2e2adb0fb873f5e1de3f23
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-25 22:08:43 +0100

    bg--updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const initDb = require('./config/db');
 initDb();
 const app = express();
 
-/**
+/*** 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
