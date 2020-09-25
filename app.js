e430baeb5cba9f5dd2745c1d2b1cdf8372b4c6e0
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-25 06:49:47 +0100

    bg--cors fixed

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const initDb = require('./config/db');
 initDb();
 const app = express();
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
 app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(bodyParser.json());
