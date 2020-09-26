132c8e875d37a1a3aa164224982d54492ae430a2
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-26 04:30:48 +0100

    ft--updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const initDb = require('./config/db');
 
 initDb();
